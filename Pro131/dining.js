eb1e844cd3da0123deb3dda2e33dfbc228f38122
Godzilla2023 <91707752+Godzilla2023@users.noreply.github.com>
2024-02-02 20:01:53 -0500

    Add files via upload

---
+++ Pro131/dining.js
@@ -0,0 +1,30 @@
+Status = "";
+dine_img = "";
+
+function preload(){
+    dine_img = loadImage("dine.webp");
+}
+
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(dine_img,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
+}
+
+function draw(){
+    image(dine_img,0,0,640,350);
+}
---
+++ Pro131/main.js
@@ -0,0 +1,15 @@
+function kitchen() {
+    window.open("fruits.html")
+}
+function bed() {
+    window.open("bed.html")
+}
+function living() {
+    window.open("living.html")
+}
+function dining() {
+    window.open("dining.html")
+}
+function bath() {
+    window.open("bath.html")
+}
